7c0be4b7deea27ab628601dc83d4ce5222cb03a9
tonnii01 <toonnnii0049@gmail.com>
2026-01-03 18:32:21 +0000

    Add Student repository

---
+++ backend/backend/src/main/java/com/example/app/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.app.repository;
+
+import com.example.app.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
